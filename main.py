f4aa1fa9050690b47919d967bbf9d42b6ff5dd77
CosmicOppai <cosmicoppai@protonmail.com>
2024-02-26 21:01:41 +0530

    added the function to plot graph

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import List, Tuple
 from sys import argv
 import logging
 from apachelogs import LogParser
+from utils.plot import plot_time_vs_status
 
 
 logging.basicConfig(filename='apache_parsing.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -9,7 +10,7 @@ logging.basicConfig(filename='apache_parsing.log', level=logging.ERROR, format='
 PARSER = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
 
 
-def parse_apache_log(logs: List[str]) -> List[Tuple[str, str]]:
+def parse_apache_log(logs: List[str]) -> List[Tuple[int, int]]:
     log_output = []
 
     for log in logs:
@@ -26,11 +27,18 @@ def parse_apache_log(logs: List[str]) -> List[Tuple[str, str]]:
     return log_output
 
 
+def _parse_args(_argv: List[str]) -> None:
+    ...
+
+
 if __name__ == "__main__":
     # read file name from command line
     file_name = argv[1]
+    output_file_name = argv[2]
+    time_window = int(argv[3]) if len(argv) > 3 else 24
     file_data = None
     with open(file_name, "r") as file:
         file_data = file.read()
-    log_data = parse_apache_log(file_data.split("\n"))
-    print(log_data)
+    parsed_log_data = parse_apache_log(file_data.split("\n"))
+    parsed_log_data = list(filter(lambda x: x[0] != "ERROR" and x[1] != "ERROR", parsed_log_data))
+    plot_time_vs_status(parsed_log_data, output_file_name)
---
+++ utils/plot.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
+import datetime
+
+
+def plot_time_vs_status(data: List[Tuple[int, int]], output_file_name: str, time_window: int = 24) -> None:
+    timestamps, http_status_codes = zip(*data)
+
+    current_time = 1707499900  # int(time.time())  # current time in seconds since the epoch, as an integer
+
+    filtered_data = [(ts, code) for ts, code in zip(timestamps, http_status_codes) if (ts < current_time and current_time - ts <= time_window * 3600)]
+
+    # Count the occurrences of each HTTP status code at each timestamp
+    status_counts = defaultdict(lambda: np.zeros(time_window*30))  # no of 2 minute intervals in time_window hours
+
+    for ts, code in filtered_data:
+        try:
+            time_diff = current_time - ts
+            time_index = int((time_window * 3600 - time_diff) / 120)
+            status_counts[code][time_index] += 1
+        except Exception as e:
+            print(datetime.datetime.fromtimestamp(ts), datetime.datetime.fromtimestamp(current_time))
+            print(time_index, time_diff, e, code)
+
+    fig, ax = plt.subplots()
+
+    x = [datetime.datetime.fromtimestamp(ts) for ts in np.arange(current_time - time_window * 3600, current_time, 120)]
+    for code, counts in status_counts.items():
+        ax.plot(x, counts, label=f'HTTP {code}')
+
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Status Count')
+    ax.set_title(f'HTTP Status Codes vs. Time in the Last {time_window} Hours')
+    ax.legend()
+
+    # Save the plot as a PNG file
+    plt.savefig(f'{output_file_name}.png')
+
+    # Show the plot
+    # plt.show()
+
+
+
+
