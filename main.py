c25f4a715c8f5f566659d9d42d0d4fa712209500
CosmicOppai <cosmicoppai@protonmail.com>
2024-02-26 16:23:29 +0530

    used apachelogs library

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from typing import List, Tuple
 from sys import argv
-from datetime import datetime
+import logging
+from apachelogs import LogParser
+
+
+logging.basicConfig(filename='apache_parsing.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+PARSER = LogParser("%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
 
 
 def parse_apache_log(logs: List[str]) -> List[Tuple[str, str]]:
     log_output = []
 
     for log in logs:
-
         try:
-
-            # Split the log line by spaces
-            parts = log.split()
-            # Extract timestamp,  and status code
-            # convert the timestamp into time since epoch
-            timestamp = datetime.strptime(parts[3][1:] + ' ' + parts[4][:-1], '%d/%b/%Y:%H:%M:%S %z').timestamp()
-            status_code = parts[8]
+            parsed_log = PARSER.parse(log)
+            timestamp: int = int(parsed_log.request_time.timestamp())
+            status_code: int = parsed_log.final_status
             log_output.append((int(timestamp), int(status_code)))
         except Exception as e:
-            print(e)
+            logging.error(f"Error parsing log line: {log} - {e}")
             log_output.append(("ERROR", "ERROR"))
             continue
 
